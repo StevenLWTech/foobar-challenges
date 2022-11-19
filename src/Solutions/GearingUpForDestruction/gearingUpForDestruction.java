9aa706e3b57ad29b2fc103f773457a87dd166265
stevenlwtech <s-steven.truong@lwtech.edu>
2022-11-19 05:40:03 -0600

    added comments to gearingupfordestruction

--- src/Solutions/GearingUpForDestruction/gearingUpForDestruction.java
+++ src/Solutions/GearingUpForDestruction/gearingUpForDestruction.java
@@ -2,8 +2,43 @@ package Solutions.GearingUpForDestruction;
 
 public class gearingUpForDestruction {
     public static int[] solution(int[] pegs) {
-        int numeratorGear = 0;
+        // Gear trains teeth much touch each other so the radius of
+        // gear0 + gear1 = b/t gear0+gear1 (distance0)
+        // gear1 = distance0 - gear0
+
+        // gear1 + gear2 = b/t gear1+gear2 (distance1)
+        // gear2 = distance1 - gear1 which is (distance0-gear0)
+        // gear2 = distance1 - distance0 + gear0
+
+        // gear2 + gear3 = distance2
+        // gear3 = distance2 - distance1 + distance0 - gear0
+
+        // the prompt says last gear runs twice as fast as the first gear
+        // so the last gear is half the size of the first gear
+        
+        // gear3 = gear0 / 2
+
+        // if gear3 = 3
+        // (gear3 = distance2 - distance1 + distance0 - gear0)
+        // (3)(gear0 / 2) = d2 - d1 + d0 - gear0
+        // (3)(gear0 / 2) + gear0 = d2 - d1 + d0
+        // (3)gear0 + gear0 = 2(d2 - d1 + d0)
+        // (3)2gear0 = (d2 - d1 + d0)
+        // gear0 = 2(d2 - d1 + d0) / 3
+        // gear0 = 2/3 (d2-d1+d0)
+
+        // if lastGear = 4
+        // (gear4 = distance3 - distance2 + distance1 - distance0 + gear0)
+        // g0/2 - g0 = (d3 - d2 + d1 - d0)
+        // g0 - g0 = 2 (d3 - d2 + d1 - d0)
+        // g0 = 2
+        
+        // therefore even numbered pegs has a ratio of n/1 and odd has a ratio fo n/3
+        // raising -1 to odd pegs gives (-1*2)+1 = 1
+        // raising 1 to even pegs gives (1*2)+1 =  3
+
         int denominatorGear = Math.abs((int) Math.pow(-1, pegs.length) * 2 + 1);
+        int numeratorGear = 0;
     
         for (int i = 1; i < pegs.length; i++) {
           numeratorGear = pegs[i] - pegs[i - 1] - numeratorGear;
--- src/Solutions/GearingUpForDestruction/test.java
+++ src/Solutions/GearingUpForDestruction/test.java
@@ -4,14 +4,17 @@ import javax.print.event.PrintEvent;
 
 public class test {
     public static void main(String[] args) {
-
-        printTest(gearingUpForDestruction.solution(new int[] { 4, 30, 50 })); //(12, 1) base case
+        // The list pegs will be given sorted in ascending order and will contain at
+        // least 2 and no more than 20 distinct positive integers, all between 1 and
+        // 10000 inclusive.
+        printTest(gearingUpForDestruction.solution(new int[] { 4, 30, 50 })); // (12, 1) base case
         printTest(gearingUpForDestruction.solution(new int[] { 4, 17, 50 })); // (-1, -1) impossible case
         printTest(gearingUpForDestruction.solution(new int[] { 1, 10000 })); // (6666, 1) min & max case 2 pegs
         printTest(gearingUpForDestruction.solution(new int[] { 4, 30, 50, 60 })); // (32, 3) even pegs
-        printTest(gearingUpForDestruction.solution(new int[] { 4, 30, 50, 60, 70 })); //(12, 1) odd pegs
+        printTest(gearingUpForDestruction.solution(new int[] { 4, 30, 50, 60, 70 })); // (12, 1) odd pegs
     }
+
     public static void printTest(int[] arr) {
-        System.out.println(arr[0] + ", " + arr[1]);
+        System.out.println("The first gear's radius is " + arr[0] + " / " + arr[1] + " in fraction form");
     }
 }
