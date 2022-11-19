ede5548b03056280fdf5a130f0aa1d01f02f2708
stevenlwtech <s-steven.truong@lwtech.edu>
2022-11-18 21:28:40 -0600

    Initial commit of all files to the repository

---
+++ src/Solutions/reId/reId.java
@@ -0,0 +1,26 @@
+package Solutions.reId;
+
+public class reId {
+    public static String solution(int i) {
+        int count, checkNum = 2;
+        String prime = "";
+        //max case + 5 digits
+        while (prime.length() < 10000 + 5) {
+            count = 0;
+            for (int idx = 1; idx <= checkNum; idx++) {
+                if (checkNum % idx == 0) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                prime += checkNum;
+            }
+            checkNum++;
+        }
+        String newMinionId = "";
+        for (int j = i; j < i + 5; j++) {
+            newMinionId += prime.charAt(j);
+        }
+        return newMinionId;
+    }
+}
---
+++ src/Solutions/reId/test.java
@@ -0,0 +1,8 @@
+package Solutions.reId;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(reId.solution(0)); // (23571) min case
+        System.out.println(reId.solution(10000)); // (02192) max case
+    }
+}
