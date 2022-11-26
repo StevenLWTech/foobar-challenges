e6c4c060869700b22139cc733df3f41511872b9d
stevenlwtech <s-steven.truong@lwtech.edu>
2022-11-25 20:02:24 -0600

    added PrepareTheBunniesEscape

---
+++ src/Solutions/PreparetheBunniesEscape/test.java
@@ -0,0 +1,12 @@
+package Solutions.PreparetheBunniesEscape;
+
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println(prepareTheBunniesEscape
+                .solution(new int[][] { { 0, 1, 1, 0 }, { 0, 0, 0, 1 }, { 1, 1, 0, 0 }, { 1, 1, 1, 0 } }));
+        System.out.println(prepareTheBunniesEscape.solution(new int[][] { { 0, 0, 0, 0, 0, 0 }, { 1, 1, 1, 1, 1, 0 },
+                { 0, 0, 0, 0, 0, 0 }, { 0, 1, 1, 1, 1, 1 }, { 0, 1, 1, 1, 1, 1 }, { 0, 0, 0, 0, 0, 0 } }));
+
+    }
+}
